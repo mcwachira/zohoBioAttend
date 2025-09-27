ef9a496ee00c09892be072b155b872c0c53d6e15
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-09-27 23:55:59 +0300

    Added some routes for the worker and site

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,7 +3,7 @@ import "dotenv/config"
 import cors from "cors";
 
 import bodyParser from "body-parser";
-import attendanceRoutes from "./routes/attendance.routes";
+import mainRoutes from "./routes/index.routes";
 
 const app  = express()
 app.use(bodyParser.json())
@@ -17,7 +17,7 @@ const ZOHO_APP = process.env.ZOHO_APP!;
 const ATTENDANCE_FORM = process.env.ATTENDANCE_FORM!;
 
 // Register routes
-app.use("/api", attendanceRoutes);
+app.use("/api", mainRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
--- api/src/routes/index.routes.ts
+++ api/src/routes/index.routes.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import { checkIn, checkOut } from "../controllers/attendance.controller";
+import { getWorkers } from "../controllers/worker.controller";
+import { getSites } from "../controllers/site.controller";
 
 const router = Router();
 
+//attendance
 router.post("/checkin", checkIn);
 router.post("/checkout", checkOut);
+
+// workers & sites
+router.get("/workers", getWorkers);
+router.get("/sites", getSites);
+
 
 export default router;
