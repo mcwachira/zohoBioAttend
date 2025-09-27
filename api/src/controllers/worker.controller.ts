ee8a4a0250e8565e36a131ed1a99f1fa47aa6362
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-09-27 23:54:52 +0300

    Created the Worker Controller

---
+++ api/src/controllers/worker.controller.ts
@@ -0,0 +1,18 @@
+import {Request, Response} from "express";
+import {fetchFormRecords}    from "../services/zoho.service";
+
+
+export const getWorkers = async (req: Request, res: Response) => {
+    try{
+        const workers = await fetchFormRecords('Worker_Report');
+        const formatted = workers
+            .filter((w: any) => w.Status === "Active")
+            .map((w: any) => ({
+                id: w.ID,
+                name: `${w.Worker_Name?.zc_display_value || `${w.Worker_Name?.first_name} ${w.Worker_Name?.last_name}`} (${w.Role})`,
+            }))
+        res.status(200).json(formatted)
+    }catch(err:any) {
+        res.status(500).json({error:err.message})
+    }
+}
